06cbc0541110365d0ba275237e86951afa22cf04
Andre Panizza <andredevops7@gmail.com>
2023-12-22 13:38:58 -0300

    update

--- PHP/exemplo-01.php
+++ PHP/exemplo-01.php
@@ -1,11 +1,7 @@
 <?php
 
 $conn = new PDO("mysql:local=localhost;dbname=dbphp7","root","");
-if($conn == Mysqli_connect_ernno()):
-    throw new Exception("<center><b>Não foi possivel conectar ao Banco de Dados</b></center>", 1);
-else:
-    echo strtoupper("<center> <b> Conectado com Sucesso ao Banco de Dados </b> </center>");
 
-endif;
+
     
 ?>
