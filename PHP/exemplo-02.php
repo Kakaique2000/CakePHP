b8360f9ddecb19406dc08b40510a8134df28c290
Andre Panizza <andredevops7@gmail.com>
2023-12-22 14:09:22 -0300

    update

--- PHP/exemplo-02.php
+++ PHP/exemplo-02.php
@@ -1,6 +1,10 @@
 <?php
 /** @param Exemplo-02 em PHP: */
 $conn = new PDO("mysql:local=localhost;dbname=db_market","root","");
+if(isset($conn)):
+    echo ucwords("<center><b> Conectado com sucesso ao Banco </b></center>");
+endif;
+$stmt = $conn->prepare("INSERT INTO estoque () VALUES ()");
 
 
 
