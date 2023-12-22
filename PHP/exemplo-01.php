e9fcfe2b067026c340da8fd03ee4567359826895
Andre Panizza <andredevops7@gmail.com>
2023-12-22 14:40:52 -0300

    update

--- PHP/exemplo-01.php
+++ PHP/exemplo-01.php
@@ -8,18 +8,20 @@ endif;
 
 $stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (:LOGIN, :PASSWORD)");
 // $stmt = $conn->prepare("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID");
+// $stmt = $conn->prepare("DELETE tb_usuarios WHERE idusuario = :ID");
 $login = 'joao';
 $senha = 'querty';
 $id = 2; // O id e igual a 02 no registro do banco;
-
+# id = 1;
 # vamos fazer um bindParam para cada um das variaveis;
 $stmt->bindParam(":LOGIN",$login);
 $stmt->bindParam(":PASSWORD",$senha);
 $stmt->bindParam(":ID",$id);
-
+# mantemos $stmt->bindParam(":ID", $id);
 $stmt->execute();
 
 echo "Inserido OK!";
 // echo "Atualizado com sucesso!";
+// echo "Deletado com sucesso";
 
 ?>
