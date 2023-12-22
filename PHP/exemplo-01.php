df0cab1bf95b305df6cca77ad2aafadc5a89c172
Andre Panizza <andredevops7@gmail.com>
2023-12-22 13:38:25 -0300

    update

--- PHP/exemplo-01.php
+++ PHP/exemplo-01.php
@@ -8,36 +8,3 @@ else:
 
 endif;
     
-$stmt = $conn->prepare("INSERT INTO (deslogin, dessenha) VALUES (:ID , :SENHA)");
-// $conn->query("SELECT * FROM tb_usuarios GROUP BY idusuarios");
-// $stmt = $conn->prepare("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID");
-// $stmt = $conn->prepare("DELETE FROM tb_usuarios WHERE idusuario = :ID");
-// $stmt = $conn->query()
-
-$login = "user";
-$senha = "1234566";
-
-$stmt->bindParam(":ID", $login);
-$stmt->bindParam(":SENHA", $senha);
-$stmt->bindParam("ID",$id);
-$stmt->execute();
-echo ucwords("Inserido  com Sucesso");
-echo "<hr>";
-
-// Estrutura do banco de Dados:
-/** @param SQL:
- * CREATE DATABASE dbphp7;
- * USE dbphp7;
- * CREATE TABLE tb_usuarios(
-* id INT(1)NOT NULL PRIMARY KEY AUTO_INCREMENT,
-* deslogin VARCHAR(45) NOT NULL,
-* dessenha VARCHAR(255) NOT NULL,
-* dtcadastro  CURRENT_TIMESTAMP();
-* );
-* # FAZENDO UM SELECT NA TABELA tb_usuarios
-* SELECT * FROM tb_usuarios ORDER BY idusuarios
-*  *
- */
-
- 
-?>
