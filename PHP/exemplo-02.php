5148819f9fd1f31f7602eb965e2a0c4339e7b755
Andre Panizza <andredevops7@gmail.com>
2023-12-26 15:05:36 -0300

    update

--- PHP/exemplo-02.php
+++ PHP/exemplo-02.php
@@ -36,7 +36,7 @@ $conn = new PDO("mysql:local=localhost;dbname=db_market","root","");
 if(isset($conn)):
     echo ucwords("<center><b> Conectado com sucesso ao Banco </b></center>");
 endif;
-$stmt = $conn->prepare("INSERT INTO estoque (marca, fornecedor) VALUES (:MARCA, :FORNECEDOR)");
+# $stmt = $conn->prepare("INSERT INTO estoque (marca, fornecedor) VALUES (:MARCA, :FORNECEDOR)");
 
 
 
