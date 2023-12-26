43b5b050e4c52974897b4bafaf789af5801aa857
Andre Panizza <andredevops7@gmail.com>
2023-12-26 15:07:24 -0300

    update

--- PHP/exemplo-02.php
+++ PHP/exemplo-02.php
@@ -33,8 +33,8 @@ DROP DATABASE db_market;
 */
 /** @param Exemplo-02 em PHP: */
 $conn = new PDO("mysql:local=localhost;dbname=db_market","root","");
-if(isset($conn)):
-    echo ucwords("<center><b> Conectado com sucesso ao Banco </b></center>");
+if($conn == mysql_connect_error()):
+    return "Do not connect a Database " . mysql_connect_error();
 endif;
 # $stmt = $conn->prepare("INSERT INTO estoque (marca, fornecedor) VALUES (:MARCA, :FORNECEDOR)");
 
